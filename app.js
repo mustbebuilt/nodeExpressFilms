633c9909980981428ba505218771e2243c9a28f8
mustbebuilt <martin@mustbebuilt.co.uk>
2016-05-12 14:55:08 +0100

    make it better

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-//var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
@@ -22,9 +22,9 @@ var updateFilm = require('./routes/updateFilm');
 
 var app = express();
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
 
 // // to use default ejs layout
 // // view engine setup
@@ -33,7 +33,7 @@ app.set('view engine', 'ejs');
 
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
