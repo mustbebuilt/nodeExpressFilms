0c4a70fd9c0b4f879cd621a6b73d0a3871934737
mustbebuilt <martin@mustbebuilt.co.uk>
2016-05-12 14:23:54 +0100

    make it better

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 //var db = monk('localhost:27017/nodeFilms');
-var dbURL = process.evn.MONGODB_URI || 'mongodb://localhost:27017/nodeFilms';
+var dbURL = process.env.MONGODB_URI || 'mongodb://localhost:27017/nodeFilms';
 mongoose.connect(dbURL, function(){
 
 	console.log('Mongoose on');
