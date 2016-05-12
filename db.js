cb947283e5e5b78df0117807c2ab599161339af5
mustbebuilt <martin@mustbebuilt.co.uk>
2016-05-12 14:11:40 +0100

    make it better

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 //var db = monk('localhost:27017/nodeFilms');
-var dbURL = 'mongodb://heroku_q3x5d4vq:8od4fmkbd3j2j12rajb2ubsktv@ds021182.mlab.com:21182/heroku_q3x5d4vq' || 'mongodb://localhost:27017/nodeFilms';
+var dbURL = process.evn.MONGODB_URI || 'mongodb://localhost:27017/nodeFilms';
 mongoose.connect(dbURL, function(){
 
 	console.log('Mongoose on');
